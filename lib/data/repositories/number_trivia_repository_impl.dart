dc27b2e9b93be43f5ad2641848977e7fe98782b9
JosipTr <jtr.other13@gmail.com>
2022-09-17 13:05:10 +0200

    Create NumberTriviaRepositoryImpl class

---
+++ lib/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:number_trivia/domain/entities/number_trivia.dart';
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaRemoteDataSource _numberTriviaRemoteDataSource;
+
+  NumberTriviaRepositoryImpl(this._numberTriviaRemoteDataSource);
+
+  @override
+  Future<Either<Exception, NumberTrivia>> getRandomNumberTrivia() async {
+    try {
+      return Right(await _numberTriviaRemoteDataSource.getRandomNumberTrivia());
+    } catch (e) {
+      return Left(Exception());
+    }
+  }
+}
