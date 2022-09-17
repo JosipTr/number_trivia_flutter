ab59c3438766544f309dfefab006c11dc6a4a4ad
JosipTr <jtr.other13@gmail.com>
2022-09-17 13:09:13 +0200

    Create bloc structure

---
+++ lib/presentation/bloc/number_trivia_bloc.dart
@@ -0,0 +1,10 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  NumberTriviaBloc() : super(NumberTriviaInitial()) {
+  }
+}
