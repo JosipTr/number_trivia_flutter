059f583eeee4926702051a1aa0477460a0bd46a4
JosipTr <jtr.other13@gmail.com>
2022-09-17 12:37:28 +0200

    Create NumberTriviaRepository abstract class

---
+++ lib/domain/repositories/number_trivia_repository.dart
@@ -0,0 +1,6 @@
+import 'package:dartz/dartz.dart';
+import 'package:number_trivia/domain/entities/number_trivia.dart';
+
+abstract class NumberTriviaRepository {
+  Future<Either<Exception, NumberTrivia>> getRandomNumberTrivia();
+}
