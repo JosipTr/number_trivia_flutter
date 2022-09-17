31747c1f527c15715e4195057bf5b9eafcad6ff0
JosipTr <jtr.other13@gmail.com>
2022-09-17 12:46:18 +0200

    Create NumberTrivia model

---
+++ lib/data/models/number_trivia_model.dart
@@ -0,0 +1,19 @@
+import 'package:number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({required super.text, required super.number});
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'],
+      number: json['number'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number
+    };
+  }
+}
